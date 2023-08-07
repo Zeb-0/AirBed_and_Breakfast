994fb06d08a623373732d994ad35e62a9acdd927
Zeb-0 <morandezeb@gmail.com>
2023-08-07 23:12:53 +0300

    datetime

---
+++ my_date.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+delita = timedelta(
+        days = 50,
+        seconds = 27,
+        microseconds = 10,
+        milliseconds = 29000,
+        minutes = 5,
+        hours = 8,
+        weeks = 2
+        )
+print(delita)
